c25462b9acb3a857d27c4a487683b3186026adda
Yaroslav Dunaev <yaroslav.dunaev@gmail.com>
2020-12-03 16:41:45 +0500

    default text

--- src/modules/Layouts.ts
+++ src/modules/Layouts.ts
@@ -10,6 +10,8 @@ export interface ILayout {
 	name: LayoutNames
 	image: string
 	title: string
+	headerText: string
+	descText: string
 	description: string
 	slideImageUrl: string
 }
@@ -23,6 +25,8 @@ export const LAYOUTS: ILayout[] = [
 		name: LayoutNames.INTRO,
 		image: '',
 		title: 'Intro',
+		headerText: 'Write a title',
+		descText: 'Write a text',
 		description: 'Intro description',
 		slideImageUrl: 'https://raw.githubusercontent.com/yaroslav-dunaev/prettifiers/master/src/images/layout-bg-show.png',
 	},
@@ -30,6 +34,8 @@ export const LAYOUTS: ILayout[] = [
 		name: LayoutNames.SHOW,
 		image: '',
 		title: 'Show',
+		headerText: 'Write a heading',
+		descText: 'Write a text',
 		description: 'TEXT description',
 		slideImageUrl: 'https://raw.githubusercontent.com/yaroslav-dunaev/prettifiers/master/src/images/layout-bg-show.png',
 	},
@@ -37,6 +43,8 @@ export const LAYOUTS: ILayout[] = [
 		name: LayoutNames.TELL,
 		image: '',
 		title: 'Tell',
+		headerText: 'Write a heading',
+		descText: 'Write a text',
 		description: 'THIRD_LAYOUT',
 		slideImageUrl: 'https://raw.githubusercontent.com/yaroslav-dunaev/prettifiers/master/src/images/layout-bg-show.png',
 		//https://raw.githubusercontent.com/yaroslav-dunaev/prettifiers/master/src/images/layout-bg-tell.png
